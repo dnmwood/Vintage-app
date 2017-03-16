fbc5e743b994d9077b658c92aa3b3dba037a1ddd
DAVID <davidnmwood@gmail.com>
2017-03-16 13:45:09 -0400

    Games controller functions, model associations

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,29 @@
 class GamesController < ApplicationController
+
   def index
+    @games = Game.all
   end
 
   def new
+    @game = Game.new
+    @game.save
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if game.save
+      redirect_to 'games#index', notice: "You have added a game."
+    end
   end
 
   def show
+    @game = Game.find(params[:id])
+  end
+
+  private
+
+  def game_params
+    params.require(:games).permit(:title, :quantity)
   end
 end
