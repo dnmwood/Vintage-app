aa846daabefe3431b3964a3d56139c5b35a65ae4
Evan Shabsove <evan.shabsove@gmail.com>
2017-03-20 20:21:08 -0400

    changed to nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :games
 
-  devise_for :users
+  devise_for :users, :path => 'accounts'
+
+  resources :users do
+    resources :games
+  end
   root 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
